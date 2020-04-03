a337fb0410e4c8ea53f89a15901bba046290a2cb
Jesperwernborg <58472246+Jesperwernborg@users.noreply.github.com>
2020-04-03 11:20:11 +0200

    Add files via upload

---
+++ Resturante-jesper/assets/javascript/javascript-menu.js
@@ -0,0 +1,29 @@
+/*HamburgerNav*/
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+/*Dropdown*/
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+/*API CALL*/
